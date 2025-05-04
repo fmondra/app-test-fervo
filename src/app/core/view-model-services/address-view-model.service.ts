b78c33aaabde1c5743da6d6b003806adc4074376
fmondra <fabrizio.mondragon95@gmail.com>
2025-05-05 00:14:19 +0200

    missing chatch error

--- src/app/core/view-model-services/address-view-model.service.ts
+++ src/app/core/view-model-services/address-view-model.service.ts
@@ -1,22 +1,28 @@
 import { inject, Injectable } from "@angular/core";
 import { AddressRestService } from "../rest-services/address-rest.service";
-import { map, Observable, of } from "rxjs";
+import { catchError, map, Observable, of } from "rxjs";
 import { AddressModel, AddressViewModel, ResultModel } from "../models/address-models";
 
 @Injectable({providedIn: 'root'})
 export class AddressViewModelService {
     private restService = inject(AddressRestService);
 
-    getAddressesListByName(address: string): Observable<AddressViewModel[]>{
+    getAddressesListByName(address: string): Observable<AddressViewModel[]> {
         return this.restService.getAddressesListByName(address).pipe(
-            map((response: AddressModel) => {
-                if(!response || !response.results) return [] as AddressViewModel[];
-
-                const listConverted: AddressViewModel[] = response.results.map((model: ResultModel) => new ResultModel(model).toViewModel());
-                return this._filter(address, listConverted);
-            })
-        )
-    }
+          map((response: AddressModel) => {
+            if (!response || !response.results) return [] as AddressViewModel[];
+      
+            const listConverted: AddressViewModel[] = response.results.map(
+              (model: ResultModel) => new ResultModel(model).toViewModel()
+            );
+            return this._filter(address, listConverted);
+          }),
+          catchError((error) => {
+            console.error('Errore durante il recupero degli indirizzi:', error);
+            return of([]);
+          })
+        );
+      }
 
     private _filter(name: string, list: AddressViewModel[]): AddressViewModel[] {
         const filterValue = name.toLowerCase();
